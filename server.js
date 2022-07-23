85a29a01b94d90d48571c784b8eb2afbd0bb8f41
mabvmex <mabvmex@gmail.com>
2022-07-23 13:07:46 -0500

    server: CRUD: Post a user

--- server.js
+++ server.js
@@ -1,29 +1,53 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
-
-app.get('/', (req, res) => {
-    res.json({ message: 'alive' });
+app.get("/", (req, res) => {
+    res.json({ message: "alive" });
 });
 
 app.listen(port, () => {
     console.log(`Listening to requests on port ${port}`);
 });
 
-app.get('/explorers', async (req, res) => {
+app.get("/explorers", async (req, res) => {
     const allExplorers = await prisma.explorer.findMany({});
     res.json(allExplorers);
 });
 
-app.get('/explorers/:id', async (req, res) => {
+app.get("/explorers/:id", async (req, res) => {
     const id = req.params.id;
-    const explorer = await prisma.explorer.findUnique({ where: {id: parseInt(id)}});
+    const explorer = await prisma.explorer.findUnique({
+        where: { id: parseInt(id) },
+    });
     res.json(explorer);
-})
+});
+
+// app.post("/explorers", async (req, res) => {
+//     const explorer = {
+//         name: req.body.name,
+//         username: req.body.username,
+//         mission: req.body.mission,
+//     };
+
+//     const message = "Explorer creado";
+//     await prisma.explorer.create({ data: explorer });
+//     return res.json({ message });
+// });
+
+app.post('/explorers', async (req, res) => {
+    const explorer = {
+      name: req.body.name,
+      username: req.body.username,
+      mission: req.body.mission
+     };
+    const message = 'Explorer creado.';
+    await prisma.explorer.create({data: explorer});
+    return res.json({message});
+  });
